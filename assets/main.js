cf1881c8e73e8014263d2e57becbfc0588f185dd
Ease <106789206+easieee@users.noreply.github.com>
2025-03-04 09:40:50 +0800

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,44 @@
+const text = document.querySelector(".animate_text");
+
+const textload = () => {
+    setTimeout(() => {
+        text.textContent = "Electronics Technician";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Freelancer";
+    }, 3500);
+    setTimeout(() => {
+        text.textContent = "Frontend Developer";
+    }, 8000);
+    setTimeout(() => {
+        text.textContent = "UI/UX Designer";
+    }, 11000);
+}
+textload();
+setInterval(textload, 14455);
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
